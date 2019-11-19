1c1a4c9ceb5c71cb9c0571f12f8b1a0e5577cdc0
Gregory Keeley <gregorykeeley@pursuit.org>
2019-11-19 15:19:27 -0500

    - Updated project

--- GameOfThrones/GameOfThrones/ViewController.swift
+++ GameOfThrones/GameOfThrones/ViewController.swift
@@ -10,21 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var tableView: UITableView!
+   @IBOutlet weak var tableView: UITableView!
     var episodes = [GOTEpisode]()
     
   override func viewDidLoad() {
     super.viewDidLoad()
     tableView.dataSource = self
+    loadData()
   }
-
-
+    func loadData() {
+        episodes = GOTEpisode.allEpisodes
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let episodeDVC = segue.destination as? EpisodeDetailViewController,
+            let indexPath = tableView.indexPathForSelectedRow else {
+                fatalError("did not prepare for segue correctly")
+        }
+        let episode = episodes[indexPath.row]
+        episodeDVC.episode = episode
+    }
 }
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "leftCell", for: indexPath) as? Left_TableViewCell else {
             fatalError("Failed to dequeue for LeftCell")
         }
+        let episode = episodes[indexPath.row]
+        cell.configureCell(for: episode)
         return cell
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
