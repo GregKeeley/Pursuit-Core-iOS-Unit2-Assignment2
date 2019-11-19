7fc399f1854e48e0aa58d3171f741dda1008cd0e
Gregory Keeley <gregorykeeley@pursuit.org>
2019-11-19 15:02:42 -0500

    - Initiated Game Of Thrones Lab UI - Created Left_TableViewCell and Right_TableViewCell

---
+++ GameOfThrones/GameOfThrones/Right_TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Right_TableViewCell.swift
+//  GameOfThrones
+//
+//  Created by Gregory Keeley on 11/19/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class Right_TableViewCell: UITableViewCell {
+
+        @IBOutlet weak var episodeTitleLabel: UILabel!
+        @IBOutlet weak var episodeNumberLabel: UILabel!
+        @IBOutlet weak var seasonNumberLabel: UILabel!
+        @IBOutlet weak var episodeImage: UIImageView!
+
+        func configureCell(for episode: GOTEpisode) {
+            episodeTitleLabel.text = episode.name
+            episodeNumberLabel.text = episode.number.description
+            seasonNumberLabel.text = episode.season.description
+            episodeImage.image = UIImage(named: episode.mediumImageID)
+        }
+    }
--- GameOfThrones/GameOfThrones/ViewController.swift
+++ GameOfThrones/GameOfThrones/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tableView: UITableView!
+    var episodes = [GOTEpisode]()
+    
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    tableView.dataSource = self
   }
 
 
 }
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "leftCell", for: indexPath) as? Left_TableViewCell else {
+            fatalError("Failed to dequeue for LeftCell")
+        }
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return episodes.count
+    }
+    
+}
